375ddbd5739c4d4480e5416e5b219ed51edb19f9
xllyll_60 <yangliyin@outlook.com>
2015-04-01 09:13:30 +0800

    Initial Commit

---
+++ MessgaeViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessgaeViewDemo
+//
+//  Created by xinyu_mac on 15/4/1.
+//  Copyright (c) 2015年 xllyll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessgaeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessgaeViewDemo
+//
+//  Created by xinyu_mac on 15/4/1.
+//  Copyright (c) 2015年 xllyll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
